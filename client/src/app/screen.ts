fde725c0d638fa8845a6aa42bea98fc77f84a096
Swamisantosh <santosh_swami@magicsoftware.com>
2021-10-14 16:19:19 +0530

    'deleted'

--- client/src/app/screen.ts
+++
@@ -1,5 +0,0 @@
-import { GlobalData } from "./globaldata.service";
-import { BaseScreen, DI } from "smartux-client";
-export class Screen extends BaseScreen {
-    protected global: GlobalData = DI.get<GlobalData>(GlobalData);
-}
